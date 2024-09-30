8db89baf2f65f610b3ea90fd931a4b8bcdc35b0b
Koshelev Aleksey <koschelev00@mail.ru>
2024-09-30 17:53:27 +0300

    переделал HW-6

--- hw-6.js
+++ hw-6.js
@@ -107,3 +107,46 @@ console.log(array10);
 const sum = array10.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 const average = sum / array10.length;
 console.log(`Среднее арифметическое чисел массива: ${average}`);
+
+
+    // Game
+
+function generateTask() {
+    let operations = ["+", "-", "*", "/"]; 
+    let operation = operations[Math.floor(Math.random() * 4)];
+    let number1 = Math.ceil(Math.random() * 10);
+    let number2 = Math.ceil(Math.random() * 10); 
+      
+    switch (operation) {
+        case "+":
+        return number1 + " " + operation + " " + number2;
+        case "-": 
+        return number1 + " " + operation + " " + number2;
+        case "*": 
+        return number1 + " " + operation + " " + number2;
+        default:
+        if (number2 === 0) {
+            return generateTask();
+        }else {
+            return number1 + " / " + number2;
+            }
+        }
+      }
+
+
+let task = generateTask(); 
+let example = ("Пример:   " + task);
+alert(example); 
+let result = eval(task);
+let userAnswer = prompt('Введите ответ');
+
+function checkGuess() {
+    if (userAnswer === result.toString()) {
+      
+      alert("Ответ верный!");
+    } else {
+      alert("Ошибка!");
+     
+    }
+};
+checkGuess();
