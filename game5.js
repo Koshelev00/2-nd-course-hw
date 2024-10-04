2b950c97f6b880a6a765edf307a9328377b601fe
Alexey Koshelev <koschelev00@mail.ru>
2024-10-04 09:55:37 +0300

    сделал часть ДЗ

--- game5.js
+++ game5.js
@@ -1,27 +1,23 @@
 function game5() {
-function getComputerChoice() {
+function generationComputerAnswer() {
     return Math.floor(Math.random() * 3);
   }
-  
+  const arrAnswer = ['камень', 'ножницы', 'бумага']
+  let userAnswer = prompt("Выберите 'камень', 'ножницы' или 'бумага':").toLocaleLowerCase().trim();
 
-  let userChoice = prompt("Выберите 'камень', 'ножницы' или 'бумага':");
-  
+  let computerAnswer =arrAnswer[generationComputerAnswer()];  
 
-  let computerChoice = getComputerChoice();
-  
- 
-  if (userChoice === "камень" && computerChoice === "ножницы") {
+  if (userAnswer === arrAnswer[0] && computerAnswer === arrAnswer[1]) {
     alert("Компьютер выбрал ножницы. Вы выиграли!");
-  } else if (userChoice === "ножницы" && computerChoice === 'бумага') {
+  } else if (userAnswer === arrAnswer[1] && computerAnswer === arrAnswer[2] ) {
     alert("Компьютер выбрал бумагу. Вы выиграли!");
-  } else if (userChoice === "бумага" && computerChoice === 'камень') {
+  } else if (userAnswer === arrAnswer[2] && computerAnswer === arrAnswer[0]) {
     alert("Компьютер выбрал камень. Вы выиграли!");
   } else {
-    if (userChoice == computerChoice) {
+    if (userAnswer == computerAnswer) {
       alert("Ничья!");
     } else {
-        console.log(`Компьютер выбрал ${computerChoice}, вы выбрали ${userChoice}. Компьютер выиграл!`);
-    
-  }
+      alert(`Компьютер выбрал ${computerAnswer}, вы выбрали ${userAnswer}. Компьютер выиграл!`);
+    }
   }
 }
